c29aa734e94668445484a3ea754770f07da5d7e4
Yael TRAMIER <yael.tramier@etu.unice.fr>
2021-10-13 17:13:47 +0200

    add tests on Paire

---
+++ test/PaireTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaireTest {
+    private ArrayList<Carte> cartes;
+    private ArrayList<Integer> cartes2;
+
+    @BeforeEach
+    void setUp() {
+        Carte carte1 = new Carte("V");
+        Carte carte2 = new Carte("3");
+
+    }
+
+    @Test
+    void isPaire() {
+    }
+
+    @Test
+    void displayPairWinner() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
